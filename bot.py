c3eaacd54d8b7d7be7dccb6b8c0f70c1e2bae087
cgi-bin/ <6095048+sven-hash@users.noreply.github.com>
2023-02-06 22:38:19 +0100

    fix: no trigger bot when unknown command

--- bot.py
+++ bot.py
@@ -42,10 +42,10 @@ class TelegramBot:
         self.updater.dispatcher.add_handler(CommandHandler('mixnodes', self.getMixnodes))
         self.updater.dispatcher.add_handler(CommandHandler('m', self.getMixnodes))
         self.updater.dispatcher.add_handler(MessageHandler(Filters.text, self.unknown))
-        self.updater.dispatcher.add_handler(MessageHandler(Filters.command, self.unknown))
+        #self.updater.dispatcher.add_handler(MessageHandler(Filters.command, self.unknown))
 
         # Filters out unknown messages.
-        self.updater.dispatcher.add_handler(MessageHandler(Filters.text, self.unknown_text))
+        #self.updater.dispatcher.add_handler(MessageHandler(Filters.text, self.unknown_text))
 
         print(f"Start {__name__}")
 
